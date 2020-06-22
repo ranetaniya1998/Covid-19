0243a44b4ce34cf546b9de0fe614caf14c8b5c9c
Taniya Rane <ranetaniya1998@gmail.com>
2020-06-22 20:33:32 +0530

    Updates

---
+++ precache-manifest.26fc9d53ef96c5cc881002d2daf51ab9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a0a30d30dd7c96a6c2ef98226c8423",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "d11dc2ec28806e4deb2c",
+    "url": "/Covid-19/static/css/main.3d9ba5a2.chunk.css"
+  },
+  {
+    "revision": "641ee7aaf80d29f8453a",
+    "url": "/Covid-19/static/js/2.5b1579e4.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19/static/js/2.5b1579e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d11dc2ec28806e4deb2c",
+    "url": "/Covid-19/static/js/main.3bd0be3a.chunk.js"
+  },
+  {
+    "revision": "019811b3c8d405fae242",
+    "url": "/Covid-19/static/js/runtime-main.e6da5a53.js"
+  },
+  {
+    "revision": "9d33b8b301c8138ed2a6db08499485ce",
+    "url": "/Covid-19/static/media/covid_image1.9d33b8b3.jpg"
+  }
+]);
